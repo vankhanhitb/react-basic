f0ffa873da79daa21b852fd7900fbcf524daf00b
Joseph <vankhanhkt12b@gmail.com>
2026-08-08 09:56:40 +0700

    update server.ts file

--- MERN-Ecommerce/backend/config/db.ts
+++ MERN-Ecommerce/backend/config/db.ts
@@ -1,8 +1,17 @@
-import mongooes from "mongoose";
+import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongooes.connect(process.env.MONGOO.URL);
+    const mongoo_url = process?.env.MONGOO_URL;
+
+    if(!mongoo_url){
+      throw new Error("MONGOO_URL is not defined")
+    }
+
+    await mongoose.connect(mongoo_url);
+
+    console.log(`Connect successfully`);
+
   } catch (error) {
     console.log(`Error: ${error.message}`);
     process.exit(1);
--- MERN-Ecommerce/backend/server.ts
+++ MERN-Ecommerce/backend/server.ts
@@ -1,17 +1,22 @@
+import path from "path"
+import dotenv from "dotenv/config";
 import express from "express";
+import cookieParse from "cookie-parser";
+
+import { connectDB } from "../backend/config/db";
+
+dotenv.config();
+const PORT = Number(process.env.PORT) || 5000;
+
+connectDB();
 
 const app = express();
-const PORT = 5000;
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParse());
 
-app.get("/api/health", (_req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "MERN Ecommerce API is running",
-  });
+app.get("/", (_req, res) => {
+  console.log(`Connect to MERN`);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
